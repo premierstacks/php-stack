19a715d5ead8ad6c6183206be4e72aed2e620993
Tomáš Chochola <chocholatom1997@gmail.com>
2024-11-13 00:03:04 +0100

    Add json decodeArray and decodeObject

--- src/Encoding/Json.php
+++ src/Encoding/Json.php
@@ -21,6 +21,7 @@ declare(strict_types=1);
 namespace Premierstacks\PhpStack\Encoding;
 
 use Premierstacks\PhpStack\Debug\Errorf;
+use Premierstacks\PhpStack\Mixed\Assert;
 
 class Json
 {
@@ -36,6 +37,36 @@ class Json
         }
 
         return $data;
+    }
+
+    /**
+     * @param positive-int $depth
+     *
+     * @return array<array-key, mixed>
+     */
+    public static function decodeArray(string $json, int $depth = 512, int $flags = 0): array
+    {
+        $data = \json_decode($json, true, $depth, $flags);
+
+        if (\json_last_error() !== \JSON_ERROR_NONE) {
+            throw new \UnexpectedValueException(Errorf::unexpectedCallableError('\json_decode', [$json, true, $depth, $flags], [], \json_last_error_msg()));
+        }
+
+        return Assert::array($data);
+    }
+
+    /**
+     * @param positive-int $depth
+     */
+    public static function decodeObject(string $json, int $depth = 512, int $flags = 0): object
+    {
+        $data = \json_decode($json, false, $depth, $flags);
+
+        if (\json_last_error() !== \JSON_ERROR_NONE) {
+            throw new \UnexpectedValueException(Errorf::unexpectedCallableError('\json_decode', [$json, false, $depth, $flags], [], \json_last_error_msg()));
+        }
+
+        return Assert::object($data);
     }
 
     /**
